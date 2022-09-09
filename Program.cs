f1a9b2b8051cb947250bc369fd6cd8be2e4dd415
Brisa Figueroa <113198675+brisa2005@users.noreply.github.com>
2022-09-09 18:50:11 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿///Programa creado por Brisa Leticia Figueroa Bautista
+//Programa
+
+//Ciclo for
+//Lista de numeros del 01 al 10
+int x, y;
+Console.WriteLine("Dónde comienza el ciclo:");
+x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Dónde termina el ciclo");
+y = Convert.ToInt32(Console.ReadLine());
+
+//Desarrolamos el ciclo
+for (int i = x; i <= y; i += 4)
+{
+    Console.WriteLine("i= " + i);
+}
